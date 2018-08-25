394eb1975389d69667fd7b449edc5f554f09b889
anderson <anderdavid86@hotmail.com>
2018-08-25 17:51:57 -0500

    Se crea componente de barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { NavbarComponent } from './navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
--- src/assets/comandos_angular.sh
+++ src/assets/comandos_angular.sh
@@ -8,3 +8,17 @@
 
 #nmp install
 
+3. traer ramas remotas desde github
+
+#for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
+#git fetch --all
+#git pull --all
+
+4. Merge entre master y bootstrap 4
+
+#git merge bootstrap4
+
+5. Crear componente barra de navegacion
+
+
+
