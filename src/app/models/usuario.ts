7580daa46ddc5668d34216f74eb87962f81d1ace
anderson <anderdavid86@hotmail.com>
2018-08-25 19:29:00 -0500

    Se crea modelo usuarios

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,14 @@
+export class Usuario {
+	constructor(
+		public id:string,
+		public nombre:string,
+		public apellido:string,
+		public fechacNacimiento:string,
+		public edad:string,
+		public genero:string,
+		public ciudad:string,
+		public email:string,
+		public login:string,
+		public password:string){
+		}
+}
