84f8489eab7bb091b82c1254b21f01f2be23ca37
anderson <anderdavid86@hotmail.com>
2018-08-25 22:48:07 -0500

    Se integra tabla de visualizar usuarios con modelo usuarios

--- src/app/view-user/view-user.component.ts
+++ src/app/view-user/view-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
+import { Usuario } from '../models/usuario';
 
 @Component({
   selector: 'app-view-user',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-user.component.css']
 })
 export class ViewUserComponent implements OnInit {
+  
+  usuarios:Usuario[] =[];
+  usuarioSelected:Usuario;
 
   constructor() { }
 
   ngOnInit() {
+
+  	this.usuarios = [
+  		new Usuario("1","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"),
+  		new Usuario("2","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"),
+  		new Usuario("3","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf"),
+  		new Usuario("4","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf","sdfsdf")
+
+  	];
   }
+
+  onSelected(usuario:Usuario){
+  	this.usuarioSelected =usuario;
+  }
+
+
 
 }
