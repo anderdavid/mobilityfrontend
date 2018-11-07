572544f9713d611bc3789f36818c1b651f5d690b
anderson <anderdavid86@hotmail.com>
2018-11-06 19:01:59 -0500

    Se añade valicion a formulario de creacion de usuario

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -10,20 +10,6 @@ import { UsuarioService } from '../services/usuario.service';
 })
 export class CreateUserComponent implements OnInit {
 
- constructor(public rest:UsuarioService, private route: ActivatedRoute,
-               private router:Router) { 
-  }
-
-  nombreEmpty=false;
-  apellidoEmpty=false;
-  fechaEmpty=false;
-  edadEmpty=false;
-  generoEmpty=false;
-  ciudadEmpty=false;
-  emailEmpty=false;
-  loginEmpty=false;
-  passwordEmpty=false;
-
   Ciudades = ["Bogota","Medellin","Cali","Pereira","Armenia","Pasto","Ipiales"];
   years = ["1900","1901","1902","1903","1904","1905","1906","1907","1908","1909","1910","1911","1912","1913","1914","1915","1916","1917","1918","1919","1920","1921","1922","1923","1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945","1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004"];
   months=["01","02","03","04","05","06","07","08","09","10","11","12"];
@@ -33,6 +19,16 @@ export class CreateUserComponent implements OnInit {
   month="01";
   day="01";
   date="";
+  
+  nombreEmpty=false;
+  apellidoEmpty=false;
+  fechaEmpty=false;
+  edadEmpty=false;
+  generoEmpty=false;
+  ciudadEmpty=false;
+  emailEmpty=false;
+  loginEmpty=false;
+  passwordEmpty=false;
 
   vacio=false;
 
@@ -45,11 +41,21 @@ export class CreateUserComponent implements OnInit {
 
   existUserFlag =false;
   msgError="Error";
-  
+
+ constructor(public rest:UsuarioService, private route: ActivatedRoute,
+               private router:Router) { 
+  }
+
   ngOnInit() {
-    this.usuario.genero="M";
+	
+	this.usuario.genero="M";
     this.usuario.ciudad="Bogota";
     this.usuario.edad="undefine";
+  }
+
+  createUser(){ 
+    console.log("createUser()");
+    alert("createUser");
   }
 
   addUser() {
@@ -58,16 +64,15 @@ export class CreateUserComponent implements OnInit {
     console.log("crear "+this.usuario.nombre);
   	this.mData = [];
 
-    if(this.validateUser()){
+  	if(this.validateUser()){
 
-      this.rest.addUser(this.usuario).subscribe(
+  		this.rest.addUser(this.usuario).subscribe(
         (data:{})=>{
           console.log(data);
           this.mData=data;
           status =this.mData.status;
 
-
-          if(status=="true"){
+		  if(status=="true"){
             this.router.navigate(['/view']);
           }else if(status=="false"){
             this.showExistUser();
@@ -80,9 +85,7 @@ export class CreateUserComponent implements OnInit {
 
 
         );
-    }
-  	
-  	
+  	}
   }
 
   validateUser(){
@@ -163,6 +166,16 @@ export class CreateUserComponent implements OnInit {
 
   }
 
+  showExistUser(){
+    this.existUserFlag=true;
+    console.log("showExistUser()  deleteUserFlag: "+this.existUserFlag);
+  }
+  closeAlertExistUser(){
+  	this.existUserFlag=false;
+    console.log("closeAlertExistUser()  deleteUserFlag: "+this.existUserFlag);
+  }
+
+
   onSelectYear(year){
     console.log(year);
     this.year =year;
@@ -198,14 +211,6 @@ export class CreateUserComponent implements OnInit {
     return edad;
   }
 
-  showExistUser(){
-    this.existUserFlag=true;
-    console.log("showExistUser()  deleteUserFlag: "+this.existUserFlag);
-  }
-  closeAlertExistUser(){
-    this.existUserFlag=false;
-    console.log("closeAlertExistUser()  deleteUserFlag: "+this.existUserFlag);
-  }
- 
 
-}
+
+ }
